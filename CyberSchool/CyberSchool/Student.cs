e5d10b5af439a26d40435184cb9fdf7b92c82501
Noa-Mari <Noa.mari123@gmail.com>
2025-06-26 15:10:42 +0300

    added all next stge now

--- CyberSchool/CyberSchool/Student.cs
+++ CyberSchool/CyberSchool/Student.cs
@@ -8,7 +8,7 @@ namespace CyberSchool
 {
     public enum CourseType
     {
-        profession = 1, 
+        profession = 1,
         bachelor = 2,
         master = 3
     }
@@ -35,7 +35,7 @@ namespace CyberSchool
 
         public Faculty Faculty { get; private set; }
 
-        public Student(string studentFirstName, string studentLastName, int id, DateTime birthday,string maritalStatus,
+        public Student(string studentFirstName, string studentLastName, int id, DateTime birthday, string maritalStatus,
         CourseType courseType, Faculty faculty)
         {
             StudentFirstName = studentFirstName;
@@ -46,119 +46,156 @@ namespace CyberSchool
             CourseType = courseType;
             Faculty = faculty;
         }
-        public Student(){
+        public Student()
+        {
 
         }
 
         public void PrintStudent()
         {
-            System.Console.WriteLine($"Student: {StudentFirstName} {StudentLastName} \nId: {ID}\nBirthday: {Birthday}\nCourseType: {CourseType}\nFaculty: {Faculty}");
+            System.Console.WriteLine($"Student: {StudentFirstName} {StudentLastName} \nId: {ID}\nBirthday: {Birthday.Date}\nCourseType: {CourseType}\nFaculty: {Faculty}");
         }
 
         public void Register()
         {
-            System.Console.WriteLine("New student registration: ");
-            StudentFirstName = StringCheck("Enter Student First Name:");
-            StudentLastName= StringCheck("Enter Student Last Name:");
-            ID = IntIDCheck("Enter Student ID:");
+            System.Console.WriteLine("New student registration ");
+            StudentFirstName = StringCheck("Enter Student First Name");
+            StudentLastName = StringCheck("Enter Student Last Name");
+            ID = IntIDCheck("Enter Student ID");
             Birthday = DateTimeCheck("Enter Your Birthday(dd/mm/yyyy)");
-            MaritalStatus = StringCheck("Enter Marital Status:");
+            MaritalStatus = StringCheck("Enter Marital Status");
             CourseType = EnumCheck<CourseType>("Enter Course-\n1.profession\n2.bachelor\n3.master");
             Faculty = EnumCheck<Faculty>("Enter Course-\n1.ComputerScience\n2.SoftwareEngineering\n3.Cyber\n4.Mathematics\n5.engineering");
         }
 
         private string StringCheck(string msg)
         {
-            Console.WriteLine($"{msg}:");
-            string input;
-            try
+            while (true)
             {
-                input = Console.ReadLine();
-                if (string.IsNullOrEmpty(input))
+                Console.WriteLine($"{msg}:");
+                string input;
+
+                try
                 {
-                    System.Console.WriteLine("WARNING-!:you cant enter empty string!");
-                    return StringCheck(msg);
+                    input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("WARNING-!:you cant enter empty string!");
+                        continue;
+                    }
+                    else if (input.Any(char.IsDigit))
+                    {
+                        Console.WriteLine("WARNING-!:Name cant have numbers in it !");
+                        continue;
+                    }
                 }
-                else if (input.Any(char.IsDigit))
+                catch (Exception ex)
                 {
-                    System.Console.WriteLine("WARNING-!:Name cant have numbers in it !");
-                    return StringCheck(msg);
+                    Console.WriteLine("Error-!:");
+                    Console.WriteLine(ex.Message);
+                    continue;
                 }
+                return input;
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error-!:");
-                Console.WriteLine(ex.Message);
-                return StringCheck(msg);
-            }
-            return input;
         }
 
         private int IntIDCheck(string msg)
         {
-            Console.WriteLine($"{msg}:");
-            int input;
-            try
+            while (true)
             {
-                input = int.Parse(Console.ReadLine());
-                if (input.ToString().Length != 9)
-                {
-                    Console.WriteLine("WARNING-!:ID Must Be Length Of 9!");
-                    return IntIDCheck(msg);
-                }
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("WARNING-!:ID need to be is a numbers format\n-ex:123456789-");
-                return IntIDCheck(msg);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error-!:");
-                Console.WriteLine(ex.Message);
-                return IntIDCheck(msg);
-            }
-            return input;
-        }
+                Console.WriteLine($"{msg}:");
+                int input;
 
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                    if (input.ToString().Length != 9)
+                    {
+                        Console.WriteLine("WARNING-!:ID Must Be Length Of 9!");
+                        continue;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("WARNING-!:ID need to be is a numbers format\n-ex:123456789-");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error-!:");
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                return input;
+            }
+        }
         private DateTime DateTimeCheck(string msg)
         {
-            Console.WriteLine($"{msg}:");
-            DateTime input;
-            try
+            while (true)
             {
-                input = DateTime.ParseExact(Console.ReadLine(), "MM/dd/yyyy",CultureInfo.InvariantCulture, DateTimeStyles.None);
+                Console.WriteLine($"{msg}:");
+                DateTime input;
+
+                try
+                {
+                    input = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None);
+                    if (!checkAge(input))
+                    {
+                        Console.WriteLine("--You Need To Be Over 16 To Register!---");
+                        continue;
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error-!:Invalid date");
+                    continue;
+                }
+                return input;
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error-!:");
-                Console.WriteLine(ex.Message);
-                return DateTimeCheck(msg);
-            }
-            return input;
         }
 
-        private T EnumCheck<T>(string msg) where T: Enum
+        private bool checkAge(DateTime birthDate)
         {
-            Console.WriteLine($"{msg}:");
-            int input;
-            try
-            {
-                input = int.Parse(Console.ReadLine());
+            DateTime today = DateTime.Today;
 
-                if (!Enum.IsDefined(typeof(T), input))
-                {
-                    Console.WriteLine("WARNING-!:Option Not In Range!");
-                    return EnumCheck<T>(msg);
-                }
-            }
-            catch (Exception ex)
+            int age = today.Year - birthDate.Year;
+            if (birthDate > today.AddYears(-age))
             {
-                Console.WriteLine("Error");
-                Console.WriteLine(ex.Message);
-                return EnumCheck<T>(msg);
+                age--;
             }
-            return (T)Enum.ToObject(typeof(T),input);
+            return age >= 16;
+        }
+        private T EnumCheck<T>(string msg) where T : Enum
+        {
+            while (true)
+            {
+                Console.WriteLine($"{msg}:");
+                int input;
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+
+                    if (!Enum.IsDefined(typeof(T), input))
+                    {
+                        Console.WriteLine("WARNING-!:Option Not In Range!");
+                        continue;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("WARNING-!:need to be in a numbers format\n-ex:1-");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error");
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                return (T)Enum.ToObject(typeof(T), input);
+            }
         }
 
 
--- CyberSchool/CyberSchool/obj/Debug/net9.0/CyberSchool.AssemblyInfo.cs
+++ CyberSchool/CyberSchool/obj/Debug/net9.0/CyberSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbed5fe52e57322c524513c41e713313080ed0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b99df8b39257ddaf5b4967f7641fc638b219bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
