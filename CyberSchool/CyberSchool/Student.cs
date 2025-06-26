416634ce2e4987dec764c29c55633d83b9ef0bc0
Noa-Mari <Noa.mari123@gmail.com>
2025-06-26 10:37:09 +0300

    add coursytpe and faclty and a constractor

--- CyberSchool/CyberSchool/Student.cs
+++ CyberSchool/CyberSchool/Student.cs
@@ -5,12 +5,46 @@ using System.Threading.Tasks;
 
 namespace CyberSchool
 {
+    public enum CourseType
+    {
+        profession, 
+        bachelor,
+        master
+    }
+
+    public enum Faculty
+    {
+        
+    }
+
     public class Student
     {
         public string StudentFirstName { get; private set; }
         public string StudentLastName { get; private set; }
         public int ID { get; private set; }
-        public DateTime birthday { get; private set; }
-        
+        public DateTime Birthday { get; private set; }
+
+        public CourseType CourseType { get; private set; }
+
+        public Faculty Faculty { get; private set; }
+
+        public Student(string studentFirstName, string studentLastName, int id, DateTime birthday,
+        CourseType courseType, Faculty faculty)
+        {
+            StudentFirstName = studentFirstName;
+            StudentLastName = studentLastName;
+            ID = id;
+            Birthday = birthday;
+            CourseType = courseType;
+            Faculty = faculty;
+        }
+
+        public void PrintStudent()
+        {
+            System.Console.WriteLine($"studant: {StudentFirstName} {StudentLastName} \nId: {ID}\nBirthday: {Birthday}\nCourseType: {CourseType}\nFaculty: {Faculty}");
+        }
+
+
+
     }
 }
--- CyberSchool/CyberSchool/obj/Debug/net9.0/CyberSchool.AssemblyInfo.cs
+++ CyberSchool/CyberSchool/obj/Debug/net9.0/CyberSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4684e6efe40c1083c057690d2b3fe308b234669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0257d396f712e5570e9360e3eb00ce640443e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
